0b79f602e773674c10fad17d4bc6da9e2af242d0
Kenneth <k.owusu.me@gmail.com>
2021-07-19 23:06:50 +0000

    connect app to mysql database with sequelize

---
+++ database.js
@@ -0,0 +1,20 @@
+const {Sequelize} = require('sequelize');
+
+
+const sequelize = new Sequelize(
+  {
+    dialect:"mysql",
+    host:"localhost",
+    username:"root",
+    password:"",
+    database:"company"
+  }
+);
+
+sequelize.authenticate()
+.then(()=>{
+  console.log(`Database connected successfully`)
+})
+.catch((error)=>{
+  console.error(`Error - Unable to connect to the database: ${error}`);
+})
