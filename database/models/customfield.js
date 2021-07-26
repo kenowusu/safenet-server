7e24cea480c5324733b4bd48cb9975351765e472
Kenneth <k.owusu.me@gmail.com>
2021-07-26 18:34:05 +0000

    add custom field model

---
+++ database/models/customfield.js
@@ -0,0 +1,42 @@
+const sequelize = require('../config').connection;
+
+const {DataTypes,Model} = require('sequelize');
+class CustomField extends Model{
+
+}
+
+CustomField.init(
+  {
+    id:{
+      type:DataTypes.STRING,
+      allowNull:false,
+      primaryKey:true
+    },
+    name:{
+      type:DataTypes.STRING,
+      allowNull:false
+      // Name of the field
+    },
+    value:{
+      type:DataTypes.STRING,
+      allowNull:false
+      //value for the custom field
+    }
+  },
+  {
+
+    //model options
+    sequelize,
+    modelName:'custom_field',
+    timestamps:true,
+    createdAt:'created_at',
+    updatedAt:'updated_at'
+  }
+);
+
+
+CustomField.sync({
+  force:true
+})
+
+exports.User = CustomField;
