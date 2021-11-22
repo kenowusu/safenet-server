b118dda89fbc2fed9e44c9f6ad14688a9b9ecda9
Kenneth <k.owusu.me@gmail.com>
2021-11-22 17:55:13 +0000

    progress: password suggestion/generator

--- routes/passwords.js
+++ routes/passwords.js
@@ -1,11 +1,16 @@
 let express = require('express');
 let router = express.Router();
 const {createPassword} = require('../lib/password/password');
-
+const {suggestPassword} = require('../lib/password/suggestPassword');
 
 /*Create Passwords*/
 
 router.post('/create',createPassword,(req,res,next)=>{});
+
+
+
+router.post('/suggestion',suggestPassword,(req,res,next)=>{});
+
 
 
 module.exports = router;
