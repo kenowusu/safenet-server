d0ea858b3fa1ac494a46cebb770d4a618b94ff96
Kenneth <k.owusu.me@gmail.com>
2021-11-22 15:03:05 +0000

    feature: if email already exists, return

--- database/config.js
+++ database/config.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     host:process.env.DB_HOST,
     username:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
-    database:process.env.DATABASE_NAME
+    database:"company"
   }
 );
 
