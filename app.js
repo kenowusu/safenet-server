f837162d3a1c76c60940fbf235a7a2efade7f246
Kenneth <k.owusu.me@gmail.com>
2021-11-22 10:43:59 +0000

    refactor: prefix all routes with /api/

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/passwords',passwordsRouter);
-app.use('/folders',folderRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/passwords',passwordsRouter);
+app.use('/api/folders',folderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
