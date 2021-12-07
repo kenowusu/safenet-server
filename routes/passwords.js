a046e52ce1288774b4363c3cce42b4bb60a29eed
Kenneth <k.owusu.me@gmail.com>
2021-12-07 17:11:02 +0000

    progress: getUrlPasswords

--- routes/passwords.js
+++ routes/passwords.js
@@ -4,6 +4,7 @@ const {createPassword} = require('../lib/password/createPassword');
 const {suggestPassword} = require('../lib/password/suggestPassword');
 const {getPassword} = require('../lib/password/getPassword');
 const {isAuthorized} = require('../lib/authentication/isAuthorized');
+const {getUrlPasswords} = require('../lib/password/getUrlPasswords');
 
 /*Create Passwords*/
 
@@ -16,7 +17,9 @@ router.post('/suggestion',suggestPassword,(req,res,next)=>{});
 
 
 //get password
-
 router.get('/password/:passwordId',isAuthorized,getPassword,(req,res,next)=>{});
 
+
+//get url passwords when on websites
+router.get('/url',isAuthorized,getUrlPasswords,(req,res,next)=>{});
 module.exports = router;
