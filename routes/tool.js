9b4c14f7e3ee39764bd7245578f6339e05a1411f
Kenneth <k.owusu.me@gmail.com>
2022-02-02 16:19:54 +0000

    progress: import password csv file

--- routes/tool.js
+++ routes/tool.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+
+import importPassword from '../lib/tools/importPassword';
+
+
+import {upload} from '../lib/tools/importPassword';
+
+
+
+
 
 /* GET home page. */
-router.post('/import_password', function(req, res, next) {
-  return res.send('import passwords');
-});
+router.post('/import_password',upload.single('password_import'),importPassword)
 
 module.exports = router;
