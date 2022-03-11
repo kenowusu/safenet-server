9ead378037f85b95b3953b7bb1ba4f4efd44ea53
Kenneth <k.owusu.me@gmail.com>
2022-03-11 20:20:31 +0000

    delete folder

--- routes/folder.js
+++ routes/folder.js
@@ -4,7 +4,8 @@ const {createFolder} = require('../lib/folder/createFolder');
 const {editFolder} = require('../lib/folder/editFolder');
 import isAuthenticated from '../lib/authentication/isAuthenticated';
 import getFolders from '../lib/folder/getFolders';
-import getFolder from '../lib/folder/getFolder'
+import getFolder from '../lib/folder/getFolder';
+import deleteFolder from '../lib/folder/deleteFolder';
 
 // get folders 
 router.get('/',isAuthenticated,getFolders);
@@ -14,12 +15,16 @@ router.get('/',isAuthenticated,getFolders);
 router.get('/:folder_id',isAuthenticated,getFolder);
 
 
+//delete folder 
+router.delete('/:folder_id',isAuthenticated,deleteFolder);
+
+
 //create folder
 router.post('/',isAuthenticated,createFolder)
 
 
 
-//edit folder
+//edit / update  folder
 router.put('/edit/:id',isAuthenticated,editFolder);
 
 
