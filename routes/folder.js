45326b618113b57533ce59fd9c6b33059a52d1f5
Kenneth <k.owusu.me@gmail.com>
2022-03-11 20:13:09 +0000

    get Folder

--- routes/folder.js
+++ routes/folder.js
@@ -4,20 +4,23 @@ const {createFolder} = require('../lib/folder/createFolder');
 const {editFolder} = require('../lib/folder/editFolder');
 import isAuthenticated from '../lib/authentication/isAuthenticated';
 import getFolders from '../lib/folder/getFolders';
+import getFolder from '../lib/folder/getFolder'
 
 // get folders 
 router.get('/',isAuthenticated,getFolders);
 
 
+//get folder 
+router.get('/:folder_id',isAuthenticated,getFolder);
 
 
 //create folder
-router.post('/',isAuthenticated,createFolder,(req,res,next)=>{})
+router.post('/',isAuthenticated,createFolder)
 
 
 
 //edit folder
-router.put('/edit/:id',isAuthenticated,editFolder,(req,res,next)=>{})
+router.put('/edit/:id',isAuthenticated,editFolder);
 
 
 
